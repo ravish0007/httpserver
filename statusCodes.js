cf5f2a5d9badf881536100b82e57f5192b730a98
ravish0007 <ravish.nfs@gmail.com>
2022-03-17 19:38:34 +0530

    Add basic status codes

---
+++ statusCodes.js
@@ -0,0 +1,16 @@
+module.exports = {
+  200: 'OK', // RFC 7231 6.3.1
+  201: 'Created', // RFC 7231 6.3.2
+  202: 'Accepted', // RFC 7231 6.3.3
+
+  400: 'Bad Request', // RFC 7231 6.5.1
+  401: 'Unauthorized', // RFC 7235 3.1
+  403: 'Forbidden', // RFC 7231 6.5.3
+  404: 'Not Found', // RFC 7231 6.5.4
+
+  500: 'Internal Server Error', // RFC 7231 6.6.1
+  501: 'Not Implemented', // RFC 7231 6.6.2
+  502: 'Bad Gateway', // RFC 7231 6.6.3
+  503: 'Service Unavailable', // RFC 7231 6.6.4
+  505: 'HTTP Version Not Supported' // RFC 7231 6.6.6
+}
